dce45748110009c7b095c5e1f653a6beee0973e7
quinnxade <artisticaquadragon@gmail.com>
2024-11-27 15:49:43 -0600

    Uhhh, might change everything all over again dont know yet

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,57 @@
-// Scroll-triggered animation for content box
-const contentBox = document.querySelector('.content-box');
 
-function checkVisibility() {
-  const rect = contentBox.getBoundingClientRect();
-  const isVisible = rect.top < window.innerHeight && rect.bottom >= 0;
+var TxtType = function (el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = '';
+  this.tick();
+  this.isDeleting = false;
+};
 
-  if (isVisible) {
-    contentBox.classList.add('visible'); // Add animation class
+TxtType.prototype.tick = function () {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+
+  if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
   }
-}
 
-// Listen for scroll events
-window.addEventListener('scroll', checkVisibility);
+  this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
 
-// Trigger visibility check on page load
-checkVisibility();
+  var that = this;
+  var delta = 200 - Math.random() * 100;
+
+  if (this.isDeleting) { delta /= 2; }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+  }
+
+  setTimeout(function () {
+    that.tick();
+  }, delta);
+};
+
+window.onload = function () {
+  var elements = document.getElementsByClassName('typewrite');
+  for (var i = 0; i < elements.length; i++) {
+    var toRotate = elements[i].getAttribute('data-type');
+    var period = elements[i].getAttribute('data-period');
+    if (toRotate) {
+      new TxtType(elements[i], JSON.parse(toRotate), period);
+    }
+  }
+  // INJECT CSS
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+  document.body.appendChild(css);
+};
